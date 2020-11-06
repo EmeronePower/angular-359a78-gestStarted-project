f7edead33804452a17e382aa0f373b305a2e2bc9
EmeronePower <emeronepower2@gmail.com>
2020-11-06 13:16:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-359a78
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-359a78)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('Le produit a bien été partagé!');
+  }
+
+  onNotify(){
+    window.alert('Vous serrez informé quand ce produit sera en solde');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
